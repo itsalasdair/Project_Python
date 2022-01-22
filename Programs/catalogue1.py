bf49ce1a41b524985c0231f61710cc64b7d5e061
Alasdair Corton <alasdair@linuxclub.sh>
2022-01-22 13:33:59 +0000

    Final push - will add issue for formatting

--- Programs/catalogue1.py
+++ Programs/catalogue1.py
@@ -3,7 +3,7 @@
 
 # Alasdair Corton
 
-# Products and Sales Tax
+# Setting products/descriptions and cost
 
 lovely_loveseat_description = "Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white."
 lovely_loveseat_price = 254.00
@@ -14,14 +14,14 @@ stylish_settee_price = 180.50
 luxurious_lamp_description = "Luxurious Lamp. Glass and iron. 36 inches tall. Brown with cream shade."
 luxurious_lamp_price = 52.15
 
+# Sales Tax is 8.8%
 sales_tax = 0.088
 
-# Customers
 # Customer One values set to Nil until they purchase
 customer_one_total = 0
 customer_one_itemization = ""
 
-# Adjusting Customer totals
+# Adjusting Customer total
 customer_one_total += lovely_loveseat_price
 customer_one_itemization += lovely_loveseat_description
 
