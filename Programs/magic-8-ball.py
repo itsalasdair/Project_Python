27569059b5944572985a0f0230857f5cbcf7b8db
Alasdair Corton <alasdair@linuxclub.sh>
2022-01-22 20:38:23 +0000

    Formatting changes

--- Programs/magic-8-ball.py
+++ Programs/magic-8-ball.py
@@ -11,23 +11,23 @@ random_number = random.randint(1,9)
 
 # Specifying possible outcomes of randint
 if random_number == 1:
-  print(answer = "Yes- definitely.")
+  print("Yes- definitely.")
 elif random_number == 2:
-  print(answer = "It is decidedly so.")
+  print("It is decidedly so.")
 elif random_number == 3:
-  print(answer = "Without a doubt.")
+  print("Without a doubt.")
 elif random_number == 4:
-  print(answer = "Reply hazy, try again.")
+  print("Reply hazy, try again.")
 elif random_number == 5:
-  print(answer = "Ask again later.")
+  print("Ask again later.")
 elif random_number == 6:
-  print(answer = "Better not tell you now.")
+  print("Better not tell you now.")
 elif random_number == 7:
-  print(answer = "My sources say no.")
+  print("My sources say no.")
 elif random_number == 8:
-  print(answer = "Outlook not so good.")
+  print("Outlook not so good.")
 elif random_number == 9:
-  print(answer = "Very doubtful.")
+  print("Very doubtful.")
 else:
   print("Error")
 
