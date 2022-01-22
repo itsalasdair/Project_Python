066818472378d1f6315869901f8a7bb0c14f899d
Alasdair Corton <alasdair@linuxclub.sh>
2022-01-22 13:17:52 +0000

    Pushing "initials.py" and "catalogue1.py"

---
+++ Programs/catalogue1.py
@@ -0,0 +1,4 @@
+# Codecademy Project: Catalogue
+# Program to store items and values in the store
+
+# Alasdair Corton
---
+++ Programs/initials.py
@@ -0,0 +1,29 @@
+# Codecademy Project: Initials
+# Program to display name intials in block letters
+
+# Alasdair Corton
+
+
+first_letter = """
+  A
+ A A
+A   A
+AAAAA
+A   A
+A   A
+A   A
+"""
+
+second_letter = """
+ CCC
+C   C
+C
+C
+C
+C   C
+ CCC
+"""
+
+
+
+print(first_letter + second_letter)
