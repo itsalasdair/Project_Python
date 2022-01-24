d679ac0b5cc72876415e9ccb630285207db556ff
Alasdair Corton <alasdair@linuxclub.sh>
2022-01-24 20:04:14 +0000

    Basic allow/disallow

---
+++ Programs/username-login.py
@@ -0,0 +1,10 @@
+# Testing if/elif/else operators
+
+username = input("Enter your username: ")
+
+if username == "Alasdair":
+  print("Login Successful")
+elif username == "Tom":
+  print("Login Successful")
+else:
+  print("Username Incorrect.")
