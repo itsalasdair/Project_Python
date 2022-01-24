d8a65514939a37bc0b3828b6324f3aec7bc912d6
Alasdair Corton <alasdair@linuxclub.sh>
2022-01-24 20:03:37 +0000

    Fixed formatting so it would not show answer at the start of ouput

--- Programs/magic-8-ball.py
+++ Programs/magic-8-ball.py
@@ -4,33 +4,34 @@
 import random
 
 name = "Alasdair"
-question = "Will I sleep tonight?"
+question = "Am I tall?"
 answer = ""
+
 # Generating random integer
-random_number = random.randint(1,9)
+random_number = random.randint(1, 9)
 
 # Specifying possible outcomes of randint
 if random_number == 1:
-  print("Yes- definitely.")
+  answer = ("Yes - definitely.")
 elif random_number == 2:
-  print("It is decidedly so.")
+  answer = ("It is decidedly so.")
 elif random_number == 3:
-  print("Without a doubt.")
+  answer = ("Without a doubt.")
 elif random_number == 4:
-  print("Reply hazy, try again.")
+  answer = ("Reply hazy, try again.")
 elif random_number == 5:
-  print("Ask again later.")
+  answer = ("Ask again later.")
 elif random_number == 6:
-  print("Better not tell you now.")
+  answer = ("Better not tell you now.")
 elif random_number == 7:
-  print("My sources say no.")
+  answer = ("My sources say no.")
 elif random_number == 8:
-  print("Outlook not so good.")
+  answer = ("Outlook not so good.")
 elif random_number == 9:
-  print("Very doubtful.")
+  answer = ("Very doubtful.")
 else:
-  print("Error")
+  answer = ("Try again")
 
 # Outputting question and response to console
 print(name + " asks: " + question)
-print("Magic 8-Ball's answer: " + answer)
+print(answer)
